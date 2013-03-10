dd7002fc3d6261ea36edeaad00bf1fa3c3ecded2
Terje Runde <terje.runde@gmail.com>
2013-03-09 21:51:34 -0800

    Minor tweaks + parallelized.

--- get_cse222a_lecslides.sh
+++ get_cse222a_lecslides.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+if [ ! -d "cse222a-pdfs" ]; then mkdir "cse222a-pdfs"; fi
 for pdf in `curl http://cseweb.ucsd.edu/classes/wi13/cse222A-a/syllabus.html --silent | grep 222A-wi13-l | sed 's/.*\/\(222A-wi13-l[0-9]*\.pdf\)".*/\1/'`; do
     if [ ! -f cse222a-pdfs/$pdf ];
     then
-        curl http://cseweb.ucsd.edu/classes/wi13/cse222A-a/lectures/$pdf > cse222a-pdfs/$pdf;
+        curl --silent http://cseweb.ucsd.edu/classes/wi13/cse222A-a/lectures/$pdf -o cse222a-pdfs/$pdf &
     fi
 done
+wait
+echo "Done!"
